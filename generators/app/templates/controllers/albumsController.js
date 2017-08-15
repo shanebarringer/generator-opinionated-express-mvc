57050bb982b0c8d5c2508c3857307b784b5b2684
Shane Barringer <shane.barringer@outlook.com>
2017-08-15 08:52:48 -0400

    updates albums controller

--- generators/app/templates/controllers/albumsController.js
+++ generators/app/templates/controllers/albumsController.js
@@ -1,7 +1,7 @@
-const Artist = require(__dirname + '/../models/artistModel');
+const Album = require(__dirname + '/../models/albumModel');
 
 exports.index = (req, res, next) => {
-  return Artist
+  return Album
     .fetchAll()
     .then((users) => res.status(200).json(users))
     .catch((error) => res.status(404).json({error: error}))
