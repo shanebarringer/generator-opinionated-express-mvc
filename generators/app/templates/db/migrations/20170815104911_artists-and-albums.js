da02e6cd2ebe624d0409d58b231e9b3fe32fac25
Mike Hopper <drmikeh@gmail.com>
2017-08-24 08:32:41 -0400

    Fixed bug introduced from previous cleanup.

--- generators/app/templates/db/migrations/20170815104911_artists-and-albums.js
+++ generators/app/templates/db/migrations/20170815104911_artists-and-albums.js
@@ -1,19 +1,19 @@
 exports.up = function(knex) {
-  return knex.schema.createTableIfNotExists('artists', table => ({
+  return knex.schema.createTableIfNotExists('artists', table => {
     table.increments('id').primary();
     table.string('name').unique();
     table.string('genre');
     table.timestamps(true, true);
-  }))
-  .then(() => knex.schema.createTableIfNotExists('albums', table => ({
+  })
+  .then(() => knex.schema.createTableIfNotExists('albums', table => {
     table.increments('id').primary();
     table.string('title');
     table.timestamps(true, true);
-  })))
-  .then(() => knex.schema.table('albums', table => ({
-    table.integer('artist_id').unsigned()
-    table.foreign('artist_id').references('artists.id')
-  })));
+  }))
+  .then(() => knex.schema.table('albums', table => {
+    table.integer('artist_id').unsigned();
+    table.foreign('artist_id').references('artists.id');
+  }));
 };
 
 exports.down = function(knex, Promise) {
